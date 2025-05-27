1d5695be745bf5ccd14a795293e0bbc58278ea31
Jason Kreiss <jkr78@uclive.ac.nz>
2025-05-28 11:12:22 +1200

    final images

--- COSC440_project_student_release_8May/graphs.py
+++ COSC440_project_student_release_8May/graphs.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
-filename = 'loss_w_resdense_only.txt'
+filename = 'loss_w_attn.txt'
 f = open(filename, 'r')
 lines = f.readlines()
 losses = []
@@ -19,26 +19,26 @@ loss_y= range(len(losses))
 simms_y= range(0, len(ssims)*10, 10)
 
 plt.plot(loss_y, losses)
-plt.title('Loss with resdense')
+plt.title('Loss with attention')
 plt.xlabel('Epoch')
 plt.ylabel('Loss')
 # plt.yticks(np.arange(0, .002, .0002))
-plt.savefig('loss_w_rd.png')
+plt.savefig('loss_w_attn.png')
 plt.show()
 plt.plot(simms_y, ssims)
 plt.yticks(np.arange(.2, .7, .1))
-plt.title('SSIM with resdense')
+plt.title('SSIM with attention')
 plt.xlabel('Epoch')
 plt.ylabel('SSIM')
-plt.savefig('ssim_w_rd.png')
+plt.savefig('ssim_w_attn.png')
 
 plt.show()
 plt.plot(simms_y, psnrs)
 plt.yticks(np.arange(10, 25, 2))
-plt.title('PSNR with resdense')
+plt.title('PSNR with attention')
 plt.xlabel('Epoch')
 plt.ylabel('PSNR')
-plt.savefig('psnr_w_rd.png')
+plt.savefig('psnr_w_attn.png')
 
 plt.show()
 
